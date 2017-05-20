5ea5806c4a4a7b00138edd80a5cc50209f4f2926
Marlon Guerra Mulfrod <mguerramulford@gmail.com>
2017-05-20 11:02:53 -0500

    Interfaz modificada, registro(falta limpiar), listado de apartamentos termando, falta el resporte3

--- app/src/main/java/com/example/marlonguerra/altosdelalgoritmo/Datos.java
+++ app/src/main/java/com/example/marlonguerra/altosdelalgoritmo/Datos.java
@@ -78,5 +78,38 @@ public class Datos {
         return p;
     }
 
+//Reporte # 1
+    public static Apartamento ApartamentosConSombraYBalcon(Context contexto){
+
+
+        //Declarar variables
+        SQLiteDatabase db;
+        String sql, nomenclatura, metrocuadrado, precio, piso, balcom, sombra;
+        Apartamento p = null;
+        //Abrir conexión de lectura
+        PersonasSQLiteOpenHelper aux = new PersonasSQLiteOpenHelper(contexto,"DBApartamentos",null,1);
+        db = aux.getReadableDatabase();
+
+        //Cursor
+        sql ="SELECT COUNT * FROM (SELECT * " +
+             "FROM Apartamentos " +
+             "WHERE balcon = 'Balcon' AND sombra = 'Sombra');";
+        Cursor c = db.rawQuery(sql,null);
+
+        //Recorrido del cursor
+        if(c.moveToFirst()){
+            nomenclatura = c.getString(0);
+            metrocuadrado = c.getString(1);
+            precio =c.getString(2);
+            piso = c.getString(3);
+            balcom = c.getString(4);
+            sombra = c.getString(5);
+            p = new Apartamento(nomenclatura, metrocuadrado, precio, piso, balcom, sombra);
+        }
+
+        db.close();
+        return p;
+    }
+
 }
 
--- app/src/main/java/com/example/marlonguerra/altosdelalgoritmo/Reporte.java
+++ app/src/main/java/com/example/marlonguerra/altosdelalgoritmo/Reporte.java
@@ -4,22 +4,55 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Reporte extends AppCompatActivity {
     private EditText reporte1;
     private EditText reporte2;
     private EditText reporte3;
-    private EditText reporte4;
+
+    private TableLayout PromedioTamañoPorPiso;
+    private ArrayList<Apartamento> apartamentos2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporte);
-        reporte1 = (EditText)findViewById(R.id.txtsombraybalcon);
+
+        reporte1 = (EditText)findViewById(R.id.txtsomybal);
         reporte2 = (EditText)findViewById(R.id.txtapartamentocaro);
         reporte3 = (EditText)findViewById(R.id.txtapartamentogrande);
-    }
 
+        PromedioTamañoPorPiso = (TableLayout)findViewById(R.id.tblPromedioTamañoPorPiso);
+        apartamentos2 = Datos.traerPersonas(getApplicationContext());
+
+        for (int i = 0; i <apartamentos2.size() ; i++) {
+            TableRow fila = new TableRow(this);
+            TextView c1 = new TextView(this);
+            TextView c2 = new TextView(this);
+            TextView c3 = new TextView(this);
+
+
+            c1.setText(""+(i+1));
+            c2.setText(apartamentos2.get(i).getNomenclatura());
+            c3.setText(apartamentos2.get(i).getMetrocuadrado());
+
+
+            fila.addView(c1);
+            fila.addView(c2);
+            fila.addView(c3);
+
+
+            PromedioTamañoPorPiso.addView(fila);
+        }
+
+
+
+    }
     public void Reporte1(View v){
         Apartamento p;
         String balcom, sombra;;
